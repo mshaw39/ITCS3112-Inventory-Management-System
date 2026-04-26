4504083f9a117d70a21a9f3c799cea097a55a9c8
Pragya Pandya <pragyapandya.108@gmail.com>
2026-04-26 11:50:01 -0400

    implementing IOrderService and OrderService

--- Contracts/IOrderService.cs
+++ Contracts/IOrderService.cs
@@ -1,6 +1,36 @@
 ﻿namespace ITCS3112InventoryManagementSystem.Contracts;
 
+/// <summary>
+/// Processes and manages inventory order.
+/// Acts as a template for transactions related to stocks
+/// within the system.
+/// </summary>
 public interface IOrderService
 {
+    /// <summary>
+    /// Process of ordering stocks for specific item.
+    /// </summary>
+    /// <param name="itemId">
+    /// Unique item identifier number in inventory system.
+    /// </param>
+    /// <param name="quantity"> The number if items to be ordered. </param>
+    /// <remarks>
+    /// Preconditions: itemId must exist in IItemRepository
+    /// Postconditions: Stock levels are updated and notification is sent
+    /// through INotifier.
+    /// </remarks>
+    void OrderStock (int itemId, int quantity);
     
+    /// <summary>
+    /// Cancels order and updates the repository (allowing control to
+    /// manage ordering process).
+    /// </summary>
+    /// <param name="orderId">
+    /// Unique order identifier number in inventory system.
+    /// </param>
+    /// <remarks>
+    /// Preconditions: orderId must be of a valid, active order in place
+    /// Postconditions: order will be marked canceled and repo is updated
+    /// </remarks>
+    void CancelOrder(int orderID);
 }
--- Services/OrderService.cs
+++ Services/OrderService.cs
@@ -1,6 +1,26 @@
-﻿namespace ITCS3112InventoryManagementSystem.Services;
+﻿using ITCS3112InventoryManagementSystem.Contracts;
 
-public class OrderService
+namespace ITCS3112InventoryManagementSystem.Services;
+
+public class OrderService : IOrderService
 {
-    
+    private readonly INotifier _notifier;
+    public int orderID { get; set; }
+
+    public OrderService(INotifier notifier)
+    {
+        _notifier = notifier;
+    }
+
+    public void OrderStock(int itemId, int quantity)
+    {
+        Console.WriteLine($"Order #{orderID} for item {itemId} quantity {quantity}");
+        _notifier.SendNotification($"Order #{orderID} placed for item {itemId} quantity {quantity}");
+    }
+
+    public void CancelOrder(int orderID)
+    {
+        Console.WriteLine($"Order #{orderID} has been cancelled");
+        _notifier.SendNotification($"Order #{orderID} Cancelled");
+    }
 }
